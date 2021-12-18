7d25030cbff0bb128292edc59f58d50da905b10d
BrandonSev <seveste.brandon@gmail.com>
2021-12-18 21:52:31 +0100

    crud users + changement structure de la table users

--- migrations/sqls/20211207134258-users-up.sql
+++ migrations/sqls/20211207134258-users-up.sql
@@ -1,9 +1,20 @@
 /* Replace with your SQL commands */
 CREATE TABLE `users`(
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `firstname` VARCHAR (100),
-    `lastname` VARCHAR (100),
-    `email` VARCHAR (255) NOT NULL UNIQUE,
-    `password` VARCHAR (255) NOT NULL, 
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+    firstname VARCHAR(100) NOT NULL ,
+    lastname varchar(100) NOT NULL ,
+    birthday DATE NOT NULL ,
+    address VARCHAR(255) NOT NULL ,
+    postal_code int(5) NOT NULL ,
+    city varchar(150) NOT NULL ,
+    email varchar(255) NOT NULL ,
+    phone varchar(10) NOT NULL ,
+    password varchar(255) NOT NULL ,
+    adoption_date DATE NULL ,
+    picture VARCHAR(255) NULL ,
+    cotisation_payed boolean NOT NULL DEFAULT (true),
+    active boolean NOT NULL DEFAULT (false),
+    CONSTRAINT uk_users_email UNIQUE (
+        email
+    )
+);
--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@ const mainRouter = require("./routes");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ foo: "hello" });
-});
-
-app.use("/api/", mainRouter);
+app.use("/api", mainRouter);
 
 module.exports = app;
