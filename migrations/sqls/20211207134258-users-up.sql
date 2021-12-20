568125d2729e6541972d6b2eb275ecc0f7191d86
ChristelleM28 <christellemonvoisin@orange.fr>
2021-12-20 09:36:58 +0100

    user

--- migrations/sqls/20211207134258-users-up.sql
+++ migrations/sqls/20211207134258-users-up.sql
@@ -1,9 +1,22 @@
-/* Replace with your SQL commands */
 CREATE TABLE `users`(
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `firstname` VARCHAR (100),
-    `lastname` VARCHAR (100),
-    `email` VARCHAR (255) NOT NULL UNIQUE,
-    `password` VARCHAR (255) NOT NULL, 
-    PRIMARY KEY (`id`)
+    `id` int AUTO_INCREMENT NOT NULL ,
+    `firstname` varchar(100)  NOT NULL ,
+    `lastname` varchar(100)  NOT NULL ,
+    `birthday` DATE  NOT NULL ,
+    `address` varchar(255)  NOT NULL ,
+    `postal_code` varchar(5)  NOT NULL ,
+    `city` varchar(255)  NOT NULL ,
+    `email` varchar(255)  NOT NULL ,
+    `phone` varchar(10)  NOT NULL ,
+    `password` varchar(255)  NOT NULL ,
+    `adoption_date` DATE  NULL ,
+    `picture` VARCHAR(255)  NULL ,
+    `cotisation_payed` boolean  NOT NULL DEFAULT (true),
+    `status_id` int  NOT NULL ,
+    CONSTRAINT `pk_users` PRIMARY KEY (
+        `id`
+    ),
+    CONSTRAINT `uk_users_email` UNIQUE (
+        `email`
+    )
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
