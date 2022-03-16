acb3dd89cb447ca2d0aa18e950c7a396a3074eca
BrandonSev <seveste.brandon@gmail.com>
2022-03-16 16:01:18 +0100

    correction CRUD messages + ajout des tests sur le crud

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,6 +7,7 @@ const AdoptionPlaceController = require("./adoption_place.controllers");
 const ProvinceController = require("./province.controllers");
 const RoomController = require("./room.controllers");
 const UnderStatusController = require("./under_status.controllers");
+const MessageController = require("./message.controllers");
 
 module.exports = {
   UserController,
@@ -18,4 +19,5 @@ module.exports = {
   RoomController,
   AssetsController,
   UnderStatusController,
+  MessageController,
 };
