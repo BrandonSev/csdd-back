3359706dbe28758139ccb8e96112d92b56ce0852
ChristelleM28 <christellemonvoisin@orange.fr>
2022-03-16 12:06:14 +0100

    crud books

---
+++ src/models/books.model.js
@@ -0,0 +1,30 @@
+const { connection } = require("../../db-connection");
+
+class Books {
+  static findMany() {
+    const sql = "SELECT * FROM books";
+    return connection.promise().query(sql);
+  }
+
+  static findOneById(id) {
+    const sql = "SELECT * FROM books WHERE id=?";
+    return connection.promise().query(sql, [id]);
+  }
+
+  static createOne(book) {
+    const sql = "INSERT INTO books SET filename=?";
+    return connection.promise().query(sql, [book]);
+  }
+
+  static updateOneById(book, id) {
+    const sql = "UPDATE books SET ? WHERE id=?";
+    return connection.promise().query(sql, [book, id]);
+  }
+
+  static deleteOneById(id) {
+    const sql = "DELETE FROM books WHERE id=?";
+    return connection.promise().query(sql, [id]);
+  }
+}
+
+module.exports = Books;
