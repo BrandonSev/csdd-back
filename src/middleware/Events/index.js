b02fa5c9be0cfd9481069e4e05f5788486afd128
nicolas-saradin <saradinnicolas9@gmail.com>
2022-03-17 10:11:10 +0100

    Events errors

--- src/middleware/Events/index.js
+++ src/middleware/Events/index.js
@@ -2,7 +2,7 @@ const { Events } = require("../../models");
 
 const validatePostEvents = async (req, res, next) => {
   const { filename, event_date, description } = req.body;
-  if (!filename && event_date && description) return res.status(400).json({ message: "Fournissez des valeurs correctes" });
+  if (!filename && !event_date && !description) return res.status(400).json({ message: "Fournissez des valeurs correctes" });
   try {
     const [events] = await Events.findOneByName(filename);
     if (events.length) return res.status(422).json({ message: "Un rôle sous ce nom existe déjà" });
@@ -18,7 +18,7 @@ const validatePutEvents = async (req, res, next) => {
   const { id } = req.params;
   const [events] = await Events.findOneById(id);
   if (!events.length) return res.status(404).send();
-  if (!filename || event_date || description) return res.status(400).json({ message: "Fournissez des valeurs correctes" });
+  if (!filename || !event_date || !description) return res.status(400).json({ message: "Fournissez des valeurs correctes" });
   try {
     const [event] = await Events.findOneByName(filename);
     if (event.length) return res.status(422).json({ message: "Un evènements sous ce nom existe déjà" });
