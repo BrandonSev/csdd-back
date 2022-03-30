615af91cb33ba26f3ad6ffb14eaf888d39a18c0c
ChristelleM28 <christellemonvoisin@orange.fr>
2022-03-30 19:21:46 +0200

    ajout cors dans app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mainRouter = require("./routes");
 
 const app = express();
 app.use(express.json());
-
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }));
+app.use("/images", express.static("assets"));
 app.use("/api", mainRouter);
 
 module.exports = app;
