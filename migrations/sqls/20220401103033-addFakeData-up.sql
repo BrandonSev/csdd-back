764bc6a71f3f97fbd17d938fc03bc4034388e9e5
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-04-08 10:10:17 +0200

    addFakeData changed

--- migrations/sqls/20220401103033-addFakeData-up.sql
+++ migrations/sqls/20220401103033-addFakeData-up.sql
@@ -2,4 +2,4 @@
 INSERT INTO province SET name="Province 1";
 INSERT INTO adoption_place SET name="Lieu d'adoption 1";
 
-INSERT INTO users SET firstname="user", lastname="user lastname", birthday="2022-02-02", address="Adresse de test", postal_code=28000, city="La loupe", email="email@csdd.fr", phone="0200221123", password="test", cotisation_payed=0, province_id=1, adoption_place_id=1;
+INSERT INTO users SET firstname="user", lastname="user lastname", birthday="2022-02-02", address="Adresse de test", postal_code=28000, city="La loupe", email="email@csdd.fr", phone="0200221123", password="$argon2i$v=19$m=4096,t=3,p=1$+KzAQYzDOWFhB9Z46aTPDw$LQAmBcXvr/n8laZJiiQgdoI5Oy2DcKAyaXVz5RY6LEo", cotisation_payed=0, province_id=1, adoption_place_id=1, adoption_date="2022-04-07";
