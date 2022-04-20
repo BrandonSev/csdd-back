22feeda6ce9b8aafd69f7f09d260f3c79151894b
BrandonSev <seveste.brandon@gmail.com>
2022-04-20 17:34:20 +0200

    data migration

--- migrations/sqls/20220401103033-addFakeData-up.sql
+++ migrations/sqls/20220401103033-addFakeData-up.sql
@@ -1,4 +1,15 @@
 /* Replace with your SQL commands */
 INSERT INTO roles SET name="admin";
+INSERT INTO roles (name) VALUES 
+('Apprenti'),
+('Prépa TDF'),
+('Aspirant'),
+('Compagnon'),
+('Rôleur de Corporation'),
+('Maître de Stage'),
+('Chargé de Mission'),
+('Maître de Métier'),
+('Premier Compagnon'),
+('Conseil de Corporation');
 INSERT INTO users SET firstname="Webmaster", lastname="lastname", birthday="2022-02-02", address="Adresse de test", postal_code=28000, city="La loupe", email="webmaster@csdd.fr", phone="0200221123", password="$argon2i$v=19$m=4096,t=3,p=1$+KzAQYzDOWFhB9Z46aTPDw$LQAmBcXvr/n8laZJiiQgdoI5Oy2DcKAyaXVz5RY6LEo", cotisation_payed=1, adoption_date="2020-04-07", active=1;
 INSERT INTO users_roles SET users_id=1, roles_id=1;
---
+++ migrations/sqls/20220420153300-addReceptionData-down.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+SET FOREIGN_KEY_CHECKS=0;
+truncate table reception_place;
+SET FOREIGN_KEY_CHECKS=1;
